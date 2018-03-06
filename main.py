a11e1de99439ec6cf24395638f0382200064dcc5
Christine <36457194+cnn22@users.noreply.github.com>
2018-03-06 00:19:43 -0800

    Update main.py
    One major change. The way I import tkinter is not using *, i use "as tk. This will affect how you make the classes and the constructors... just fyi...

--- main.py
+++ main.py
@@ -1,33 +1,79 @@
-from tkinter import *
+import tkinter as tk
+#import tkFont
+#from PIL import ImageTk, Image
 
-class uberDesktop:
-    def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
+TITLE_FONT = ('Helvetica 50 bold')
+BUTTON_FONT = ('Arial 12 bold')
+class uberApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        uberDesktop = tk.Frame(self)
+        #uberDesktop.geometry('1000x500')
 
-        master.title("Uber Developers")
-        master.configure(background = 'white')
+        uberDesktop.configure(background='black')
+        uberDesktop.pack()
 
-        self.uberLogo = Label(frame, text = 'U B E R', bg='black', fg = 'white', font = 'Helvetica 50 bold')
-        self.uberLogo.pack()
+        #the frames!
+        self.frames = {}
+        for page in (uberLoginPage, mainMenuPage):
+            frame = page(uberDesktop, self)
+            self.frames[page] = frame
+            frame.grid(row=0, column=0, sticky='nsew')
 
-        self.email = StringVar()
-        self.emailLogin = Entry(frame, textvariable = self.email)
-        self.emailLogin.pack()
+        self.show_frame(uberLoginPage)
 
-        self.password = StringVar()
-        self.passwordLogin = Entry(frame, textvariable = self.password)
-        self.passwordLogin.pack()
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise() #raises the frame to the front...aka switch screens
 
-        self.loginButton = Button(frame, text = 'LOG IN', font = 'Helvetica 15', width = 30, command=self.login)
-        #self.loginButton.grid(row = 250, column=250)
-        self.loginButton.pack()
 
-        master.geometry("500x200+100+100")
+class uberLoginPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(background='black')
+        #logo = 'Logobit_digital_black_large.png'
+        #uberLogo = ImageTk.PhotoImage(Image.open(logo))
+        #panel = Label()
+        uberLogo = tk.Label(self, text = 'U B E R', bg='black', fg='white', font=TITLE_FONT)
+        uberLogo.pack(pady = 10, padx = 10)
 
-    def login(self):
-        print("logged in")
+        #emailString = tk.StringVar()
+        #emailLogin = tk.Entry(self, textvariable = emailString)
+        #emailLogin.pack(pady = 10, padx = 10)
+        emailBox = tk.Entry(self)
+        #emailBox.insert(0, 'Email')
+        #email.Box.bind*"<FocusIn", self.clearWidget())
+        emailBox.pack(pady=10, padx=10)
 
-uberWindow = Tk()
-my_gui = uberDesktop(uberWindow)
-uberWindow.mainloop()
+        passwordBox = tk.Entry(self, show='*')
+        passwordBox.pack(pady=10, padx=10)
+
+        loginButton = tk.Button(self, text = 'Log In', font = BUTTON_FONT, bg='#808080', fg='white', command=lambda:controller.show_frame(mainMenuPage))
+        #loginButton.bind('<return', login)
+        loginButton.pack(pady=10, padx=10)
+
+    #def login(self, cont)
+
+class mainMenuPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(background='black')
+        welcomeLabel = tk.Label(self, text = 'Welcome', fg = 'white', bg='black', font='Helvetica 18 bold')
+        displayUsername = tk.Label(self, text = 'Christine Nguyen', fg = '#00e6e6', bg='black', font='Helvetica 18')
+
+        signOutButton = tk.Button(self, text='Sign Out', command=lambda:controller.show_frame(uberLoginPage))
+        alterMenuButton = tk.Button(self, text='ALTER') #command=lambda:controller:show_frame(alterMenuPage)
+
+        welcomeLabel.pack( pady=5)
+        displayUsername.pack(pady=10)
+        signOutButton.pack(fill=tk.X)
+        alterMenuButton.pack(fill=tk.X, side='bottom')
+
+    #def signOut(self):
+    #def alterMenu
+    #def createMenu
+    #def lookupMenu
+
+
+app = uberApp()
+app.mainloop()
