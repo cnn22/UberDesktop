937fd36eaaf02ad18e5d8ca28bde8a54485223a3
tekchheang <t.chheang.k@gmail.com>
2018-03-07 11:24:25 -0800

    Incident Record
    Update to add feature like filter but currently only work with fristname. if all field is empty then the report will pull everyone.

--- driverTreeview.py
+++ driverTreeview.py
@@ -5,10 +5,13 @@ from query import *
 
 class driverRecord(Frame):
 
+    record = ""
+
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.createUI()
-        self.loadTable()
+        self.createFilterPanel()
+        self.fetchallincidentrecord()
         self.grid(sticky = (N,S,W,E))
         self.grid_rowconfigure(0, weight =1)
         self.grid_columnconfigure(0, weight = 1)
@@ -31,18 +34,27 @@ class driverRecord(Frame):
         self.treeview = tv
         self.yscrollbar = ttk.Scrollbar(self, orient='vertical', command=tv.yview)
         tv.configure(yscrollcommand=self.yscrollbar.set)
+        tv.grid(row=1,column=0)
 
         tv.grid(row=0, column=0, sticky="nsew")
         self.yscrollbar.grid(row=0, column=1, sticky='nse')
         self.yscrollbar.configure(command=tv.yview)
         self.grid_rowconfigure(0, weight =1)
         self.grid_columnconfigure(0, weight = 1)
+        
 
-    def loadTable(self):
+    def fetchallincidentrecord(self):
+        global record
         q = Query()
         record = q.fetchincidentrecord()
+        self.loadTable(record)
 
-        for i in record:
+    def resetTreeview(self):
+        for i in self.treeview.get_children():
+            self.treeview.delete(i)
+
+    def loadTable(self, r):
+        for i in r:
             username = i[0]
             firstname = i[1]
             lastname = i[2]
@@ -53,33 +65,53 @@ class driverRecord(Frame):
         
         #self.treeview.insert('','end',text='AleWi', values=('Alex', "Williams",
                                                            #'282', '2017/5/22'))
-def createFilterPanel(self):
-    lbFirstname = Label(self, text = 'Firstname')
-    lbFirstname.grid(row=0, column=1)
+        
+    def createFilterPanel(self):
+        lbFirstname = Label(self, text = 'Firstname')
+        lbFirstname.grid(row=0, column=2)
 
-    firstname_text = StringVar()
-    tbFirstname = Entry(self, textvariable=firstname_text)
-    tbFirstname.grid(row=0, column=2)
+        firstname_text = StringVar()
+        tbFirstname = Entry(self, textvariable=firstname_text)
+        tbFirstname.grid(row=0, column=3)
 
-    lbLastname = Label(self, text = 'Lastname')
-    lbLastname.grid(row=0, column=3)
+        lbLastname = Label(self, text = 'Lastname')
+        lbLastname.grid(row=0, column=4)
 
-    Lastname_text = StringVar()
-    tbLastname = Entry(self, textvariable=Lastname_text)
-    tbLastname.grid(row=0, column=4)
+        Lastname_text = StringVar()
+        tbLastname = Entry(self, textvariable=Lastname_text)
+        tbLastname.grid(row=0, column=5)
 
-    lbUsername = Label(self, text = 'Username')
-    lbUsername.grid(row=1, column=1)
+        lbUsername = Label(self, text = 'Username')
+        lbUsername.grid(row=1, column=2)
 
-    Username_text = StringVar()
-    tbUsername = Entry(self, textvariable=Username_text)
-    tbUsername.grid(row=1, column=2)
+        Username_text = StringVar()
+        tbUsername = Entry(self, textvariable=Username_text)
+        tbUsername.grid(row=1, column=3)
 
-    
+        btnSubmit = Button(self,
+                           text='Submit',
+                           command=lambda: self.submitFilter(tbFirstname.get(), tbLastname.get(), tbUsername.get()))
+        btnSubmit.grid(row=2, column=2)
+
+    def submitFilter(self, argFirstname, argLastname, argUsername):
+        global record
+        q = Query()
+        """if argUsername == "":
+            print(argUsername)
+        elif not argFirstname == "" and argLastname == "":
+            print(argLastname)"""
+        if not argFirstname == "":
+            record = q.fetchbyfirstname(argFirstname)
+
+        if argFirstname == "" and argLastname == "" and argUsername == "":
+            self.fetchallincidentrecord()
+            
+        self.resetTreeview()
+        self.loadTable(record)
+
     
 def main():
     root = Tk()
-    createFilterPanel(root)
     driverRecord(root)
     root.mainloop()
 
@@ -87,6 +119,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-#scrollbar = Scrollbar(root)
-#scrollbar.pack(side=RIGHT, fill=Y)
