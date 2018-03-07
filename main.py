d768733fd8650d54794c5c7f686abcc66a655fce
Christine <36457194+cnn22@users.noreply.github.com>
2018-03-07 11:22:50 -0800

    Update main.py
    A slight design change in the original plan related to the "Report" call. 
    When the user selects Report the app will go to another frame, and from there they will select the query they want. 
    Once they select the query, a new window will pop up and in that instance the appropriate frame will be called.

--- main.py
+++ main.py
@@ -1,79 +1,136 @@
 import tkinter as tk
-#import tkFont
-#from PIL import ImageTk, Image
+from driverTreeview import *
 
 TITLE_FONT = ('Helvetica 50 bold')
 BUTTON_FONT = ('Arial 12 bold')
+GENERAL_FONT =('Helvetica 12 bold')
+
+#uberApp is the class that handles the initalization and frames of the entire application.
+#PLEASE USE THIS WHEN TESTING AND BUILDING FRAMES!!!
 class uberApp(tk.Tk):
+    counter = 0
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         uberDesktop = tk.Frame(self)
-        #uberDesktop.geometry('1000x500')
-
+        self.geometry('1000x500')
+        self.configure(background='black')
         uberDesktop.configure(background='black')
-        uberDesktop.pack()
+        uberDesktop.pack(pady=100, padx=100)
 
-        #How I handle different frames...
+
+        #the frames are handled here. I put all my frames here...
         self.frames = {}
-        for page in (uberLoginPage, mainMenuPage):
+        for page in (uberLoginPage, mainMenuPage, reportMenu):
             frame = page(uberDesktop, self)
             self.frames[page] = frame
             frame.grid(row=0, column=0, sticky='nsew')
 
-        self.show_frame(uberLoginPage)
+        self.show_frame(uberLoginPage) #displays the loginPage after firing up the app
 
+    #show_frame is a function that will show the frame that you want to display by "raising it".
+    #there is no particular order, it raises whenever you want to raise
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise() #raises the frame to the front...aka switch screens
 
+    def create_window(self, name):
+        self.counter+=1
+        window = tk.Toplevel(self)
+        window.wm_title(name)
 
+
+
+#uberLoginPage is the frame that handles what the login page will look like
 class uberLoginPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
         self.configure(background='black')
-        #logo = 'Logobit_digital_black_large.png'
-        #uberLogo = ImageTk.PhotoImage(Image.open(logo))
-        #panel = Label()
         uberLogo = tk.Label(self, text = 'U B E R', bg='black', fg='white', font=TITLE_FONT)
         uberLogo.pack(pady = 10, padx = 10)
 
-        #emailString = tk.StringVar()
-        #emailLogin = tk.Entry(self, textvariable = emailString)
-        #emailLogin.pack(pady = 10, padx = 10)
         emailBox = tk.Entry(self)
         #emailBox.insert(0, 'Email')
         #email.Box.bind*"<FocusIn", self.clearWidget())
+        emailBox.config(width=50)
         emailBox.pack(pady=10, padx=10)
 
         passwordBox = tk.Entry(self, show='*')
+        passwordBox.config(width=50)
         passwordBox.pack(pady=10, padx=10)
 
-        loginButton = tk.Button(self, text = 'Log In', font = BUTTON_FONT, bg='#808080', fg='white', command=lambda:controller.show_frame(mainMenuPage))
+        loginButton = tk.Button(self, text = 'Log In', font = BUTTON_FONT, bg='#333333', fg='white', command=lambda:controller.show_frame(mainMenuPage)) #this is where the magic happens with the frames
         #loginButton.bind('<return', login)
-        loginButton.pack(pady=10, padx=10)
-
+        loginButton.config(width=30)
+        loginButton.pack(pady=10, padx=10, ipady=5)
     #def login(self, cont)
-
+    #def clearWidget():
+#mainMenuPage is the frame that handles what the main menu will look like
 class mainMenuPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.configure(background='black')
-        welcomeLabel = tk.Label(self, text = 'Welcome', fg = 'white', bg='black', font='Helvetica 18 bold')
-        displayUsername = tk.Label(self, text = 'Christine Nguyen', fg = '#00e6e6', bg='black', font='Helvetica 18')
 
-        signOutButton = tk.Button(self, text='Sign Out', command=lambda:controller.show_frame(uberLoginPage))
-        alterMenuButton = tk.Button(self, text='ALTER') #command=lambda:controller:show_frame(alterMenuPage)
+        self.configure(background='black')
+
+
+        #selection = tk.StringVar(self)
+        #selection.set(REPORT_OPTIONS[0])
+        #reportMenu = tk.OptionMenu(self, selection, *REPORT_OPTIONS)
+
+        welcomeLabel = tk.Label(self, text = 'Welcome', fg = 'white', bg='black', font='Helvetica 18 bold')
+        displayUsername = tk.Label(self, text = '[user]', fg = '#00e6e6', bg='black', font='Helvetica 18')
+
+        signOutButton = tk.Button(self, text='Sign Out', bg='#008080', fg='white', font = 'Arial 10', command=lambda:controller.show_frame(uberLoginPage)) #this is where the magig happens with the frames!
+        reportButton = tk.Button(self, text='REPORTS', bg='#008080', fg='white', font = BUTTON_FONT, command=lambda:controller.show_frame(reportMenu)) #command=Lambda:controller.show_frame(createMenuPage)
+
+
+        alterMenuButton = tk.Button(self, text='ALTER', bg='#008080', fg='white', font = BUTTON_FONT) #command=lambda:controller.show_frame(alterMenuPage)
+        lookupMenuButton = tk.Button(self, text='LOOK UP', bg='#008080', fg='white', font = BUTTON_FONT) #command=Lambda:controller.show_frame(lookupMenuPage)
 
         welcomeLabel.pack( pady=5)
         displayUsername.pack(pady=10)
-        signOutButton.pack(fill=tk.X)
-        alterMenuButton.pack(fill=tk.X, side='bottom')
+        signOutButton.config(width=10)
+        alterMenuButton.config(width=50)
+        #reportButton.config(width=50)
+        #reportMenu.pack()
+        lookupMenuButton.config(width=50)
 
-    #def signOut(self):
+        #signOutButton.place(relx=0.0, rely=1.0, anchor='ne')
+        signOutButton.pack()
+        reportButton.pack(pady=5, ipadx=210, ipady=5)
+        alterMenuButton.pack(pady=5, ipady=5)
+        lookupMenuButton.pack(pady=5, ipady=5)
+
     #def alterMenu
     #def createMenu
     #def lookupMenu
+#reportMenu is the frame that handles the report selections
+class reportMenu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(background='black')
 
+        instructionLabel = tk.Label(self,text="Select which report you would like to perform.", font=GENERAL_FONT,fg='white', bg='black')
+        REPORT_OPTIONS = ["Driver's Incident", "Driver's Bank", "Driver License"]
+        selection=tk.StringVar(self)
+        selection.set("--")
+        reportButton = tk.OptionMenu(self, selection, *REPORT_OPTIONS)
+        selectButton = tk.Button(self, text="Select", bg='#008080', fg='white', font = GENERAL_FONT, command=lambda:self.getQuery(selection, controller)) #command=lambd
+
+        instructionLabel.pack(pady=10)
+        reportButton.pack(padx=210, pady=10)
+        selectButton.pack()
+
+    def getQuery(self, selection, controller):
+        selection = selection.get()
+        if selection == "Driver's Incident":
+            controller.create_window("Driver's Incident")
+
+
+class alterMenuPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(background='black')
 
 app = uberApp()
 app.mainloop()
