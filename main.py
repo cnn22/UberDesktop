1931dd06c2e8e8f0abe01a94f140aba8e6c591e4
Christine <36457194+cnn22@users.noreply.github.com>
2018-03-09 08:29:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from driverTreeview import*
+from driverTreeview import *
+
 
 TITLE_FONT = ('Helvetica 50 bold')
 BUTTON_FONT = ('Arial 12 bold')
@@ -17,6 +18,7 @@ class uberApp(tk.Tk):
         uberDesktop.configure(background='black')
         uberDesktop.pack(pady=100, padx=100)
 
+
         #the frames are handled here. I put all my frames here...
         self.frames = {}
         for page in (uberLoginPage, mainMenuPage, reportMenu):
@@ -32,15 +34,12 @@ class uberApp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise() #raises the frame to the front...aka switch screens
 
-    def create_window(self, name, controller):
+    def create_window(self, name):
         self.counter+=1
         window = tk.Toplevel(self)
-        window.title(name)
+        window.wm_title(name)
         window.minsize(height=200, width=1000)
         driverIncidentRecord = driverRecord(window)
-        myDB = tk.Frame(driverIncidentRecord)
-        self.show_frame(myDB)
-
 
 #uberLoginPage is the frame that handles what the login page will look like
 class uberLoginPage(tk.Frame):
@@ -117,23 +116,16 @@ class reportMenu(tk.Frame):
         selection=tk.StringVar(self)
         selection.set("--")
         reportButton = tk.OptionMenu(self, selection, *REPORT_OPTIONS)
-        selectButton = tk.Button(self, text="Select", bg='#008080', fg='white', font = GENERAL_FONT, command=lambda:self.getTable(selection, controller)) #command=lambd
+        selectButton = tk.Button(self, text="Select", bg='#008080', fg='white', font = GENERAL_FONT, command=lambda:self.getQuery(selection, controller)) #command=lambd
 
         instructionLabel.pack(pady=10)
         reportButton.pack(padx=210, pady=10)
         selectButton.pack()
 
-    #getQuery is a method that will call create_window from the UberApp class to create a new window,
-    #and the correct frame to display the query
-    def getTable(self, selection, controller):
+    def getQuery(self, selection, controller):
         selection = selection.get()
         if selection == "Driver's Incident":
-            #controller.show_frame(driverIncident)
-            controller.create_window(selection)
-        #elif selection == "Driver's Bank":
-        #    controller.show_frame
-        #elif selection =="Driver License":
-        #    controller.show_frame
+            controller.create_window("Driver's Incident")
 
 
 class alterMenuPage(tk.Frame):
