4730cbb333c387d342fe4c6536e8061767aa8b86
tekchheang <t.chheang.k@gmail.com>
2018-03-13 10:33:55 -0700

    ExecuteSQLQuery

---
+++ Execution.py
@@ -0,0 +1,20 @@
+Class Execution():
+
+
+    def mysqlExecute(self, username, password, host, dbname, query):
+      try:
+          cnx = mysql.connector.connect(user=username, password=password, host = host,
+                                       database=dbname)
+      except mysql.connector.Error as err:
+          if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+              print("Something is wrong with your user name or password")
+          elif err.errno == errorcode.ER_BAD_DB_ERROR:
+              print("Database does not exist")
+          else:
+              print(err)
+      else:
+          cursor = cnx.cursor()
+          cursor.execute(query)
+          record = cursor.fetchall()
+          cnx.close()
+          return record
